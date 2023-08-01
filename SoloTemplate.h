70ae37f850d18d3d572839dd294643426bc05f5c
Emir0zcelik <themirozcelik@gmail.com>
2023-08-01 06:08:04 +0300

    CMakeSettings

---
+++ SoloTemplate.h
@@ -0,0 +1,12 @@
+﻿// SoloTemplate.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
