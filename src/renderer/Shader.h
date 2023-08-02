ae495d38c7eee74d61e6dd377e9b00f9004cad48
Emir0zcelik <themirozcelik@gmail.com>
2023-08-02 10:50:10 +0300

    Shader Class Implementation

---
+++ src/renderer/Shader.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+#include <glm/glm.hpp>
+
+struct ShaderProgramSource {
+
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+class Shader
+{
+private:
+	unsigned int _rendererId;
+	std::string _filePath;
+	std::unordered_map<std::string, int> _uniformLocationCache;
+public:
+
+	Shader(const std::string filepath);
+
+	~Shader();
+
+	void Bind() const;
+
+	void Unbind() const;
+
+	//Set Uniforms
+
+private:
+	ShaderProgramSource ParseShader(const std::string& filepath);
+	unsigned int CompileShader(unsigned int type, const std::string& source);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+	unsigned int GetUniformLocation(const std::string& name);
+};
